19a883b3c4070582129cc26df626101dd64b9dae
bartholomewjnr <brightasampong8@github.com>
2023-06-01 03:26:21 -0700

    adding all the files

---
+++ index.js
@@ -0,0 +1,7 @@
+function dolar() {
+document.getElementById("dolar").innerHTML = "I still have a feeling that 0.00$ is still bigger that GHC 0.00";
+}
+
+function myDolar() {
+window.alert("do you still aware that 0.00 $ is bigger than 0.00ghc")
+} 
